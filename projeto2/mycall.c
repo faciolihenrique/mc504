6cd1928ceb6c38091b6c36b120c1cb3df2ac5ee1
henriquefacioli <henrique.facioli@gmail.com>
2016-05-13 14:14:01 -0300

    Mudanca de nome para ingles

--- projeto2/mycall.c
+++ projeto2/mycall.c
@@ -2,10 +2,10 @@
 #include <linux/linkage.h>
 #include <linux/string.h>
 
-/*estrutura para guardar a chave e o endereço na memoria que o usario inseriu, para posterior busca*/
+/*estrutura para guardar a key e o endereço na memoria que o usario inseriu, para posterior busca*/
 
 typedef struct Chain{
-    struct Chain *prox;
+    struct Chain *next;
     char *cadeia;
     int key;
 } Chain;
@@ -14,40 +14,40 @@ typedef struct Chain{
 Chain *head = NULL;
 
 asmlinkage long sys_setkey(int key, char *ch){
-    Chain *elemento, *percorre = head;
+    Chain *element, *search = head;
 
-    /*aloca o elemento na memoria */
-    elemento = kmalloc(sizeof(Chain));
-    elemento->key = key;
-    elemento->cadeia = kmalloc(sizeof(char)*(strlen(ch) +1));
-    copy_from_user(ch, elemento->cadeia, sizeof(char)*(strlen(ch) +1));
-    elemento->prox = NULL;
+    /*aloca o element na memoria */
+    element = kmalloc(sizeof(Chain));
+    element->key = key;
+    element->cadeia = kmalloc(sizeof(char)*(strlen(ch) +1));
+    copy_from_user(ch, element->cadeia, sizeof(char)*(strlen(ch) +1));
+    element->next = NULL;
 
-    /*Caso seja o primeiro elemento*/
+    /*Caso seja o primeiro element*/
     if(head==NULL){
-        head = elemento;
+        head = element;
         return 1;
     }
 
     /*Faz a busca do ultimo termo*/
-    while(percorre->prox != NULL);
-    aux->prox = elemento;
+    while(search->next != NULL);
+    aux->next = element;
 
     return 1;
 }
 
-asmlinkage long sys_gettkey(int chave, char* ch){
-    Chain *percorre = head;
+asmlinkage long sys_getkey(int key, char* ch){
+    Chain *search = head;
 
     /*vai procurando ate o final da lista ligada*/
-    while (percorre != NULL) {
+    while (search != NULL) {
         /*copia o endereço do kernel, para o espaco usuario*/
-        if (percorre->key == chave) {
-            copy_to_user(ch,percorre->cadeia,sizeof(char)*(strlen(percorre->cadeia) + 1));
+        if (search->key == key) {
+            copy_to_user(ch,search->cadeia,sizeof(char)*(strlen(search->cadeia) + 1));
             return 1;
         }
-        /*continua percorrendo ate chegar no final da lista*/
-        percorre = percorre->prox;
+        /*continua searchndo ate chegar no final da lista*/
+        search = search->next;
     }
     return 0;
 }
