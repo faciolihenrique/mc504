b39eeb7964ebeb0ef1147cef2babe0c7b6ac6a63
henriquefacioli <henrique.facioli@gmail.com>
2016-06-15 01:51:06 -0300

    pequena organização

--- projeto-final/vsfs.c
+++ projeto-final/vsfs.c
@@ -1,64 +1,95 @@
-/* Código levemente modificado do hellofs
+/* VSFS
+ * Um projeto para a matéria MC504 - Sistemas Operacionais
+ * 1s2016 - Profa. Islene
+ * Grupo 6
  */
 
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
-#include <asm/uaccess.h>
+#include <linux/fs.h>           // Bibliotecas estão
+
+#include <asm/uaccess.h>        // Função copy_to_user
+
 #include "vsfs.h"
+
+/* Licença do modulo */
+MODULE_LICENSE("GPL");
+
 
 #define VSFS_MAGIC 0x19496CF8
 
+/* File operations definitions */
 static struct file_operations vsfs_file_ops = {
-    .open   = vsfs_open,
-    .read   = vsfs_read_file,
-    .write  = vsfs_write_file,
-    .release = vsfs_release,
+    .open       = vsfs_open,
+    .read       = vsfs_read_file,
+    .write      = vsfs_write_file,
 };
 
+/* vsfs[]
+ * Essa é a estrutura que está armazenando os arquivos por enquanto!
+ */
+struct tree_descr vsfs[] = {
+    { NULL, NULL, 0 },
+    { "hello.txt", &vsfs_file_ops, S_IWUSR|S_IRUGO },
+    { "", NULL, 0},
+};
+
+
+/* filesystem information */
 static struct file_system_type vsfs_type = {
     .owner          = THIS_MODULE,
     .name           = "vsfs",
-    .mount          = vsfs_get_super,
-    .kill_sb        = kill_litter_super,
+    .mount          = vsfs_mount,
+    .kill_sb        = vsfs_umount,
 };
-
-/* Essa é a estrutura da arvore de arquivos do vsfs.
- * Ele é estático por enquanto
+/* vsfs_init(void) - a 'main()'
+ * Função de inicialização do módulo
+ * Como é um filesystem deve chamar a função:
+ *      int register_filesystem(&fs_type);
+ * com o argumento:
+ *      static struct file_system_type fs_type = {
+ *          .owner 	    = THIS_MODULE,
+ *          .name	    = "lwnfs",
+ *          .get_sb	    = lfs_get_super,
+ *	        .kill_sb	= kill_litter_super,
+ *      };
  */
-struct tree_descr vsfs[] = {
-  { NULL, NULL, 0 },
-  { "hello.txt", &vsfs_file_ops, S_IWUSR|S_IRUGO },
-  { "", NULL, 0},
-};
-
-
-
 static int __init vsfs_init(void)
 {
     int reg = register_filesystem(&vsfs_type);
-    printk("VSFS properly loaded on kernel %d \n", reg);
+
+    if (likely(reg == 0))
+        printk(KERN_INFO "VSFS carregado no kernel\n", reg);
+    else
+        printk(KERN_ERR "[Erro] VSFS não carregado. REG:%d \n", reg);
+
     return reg;
 }
 
-
 static void __exit vsfs_exit(void)
 {
-    unregister_filesystem(&vsfs_type);
+    int unreg = unregister_filesystem(&vsfs_type);
+
+    if ( likely(unreg == 0))
+        printk(KERN_INFO "VSFS liberado do kernel\n");
+    else
+        printk(KERN_ERR "[Erro] VSFS não liberado. UNREG:%d", unreg);
 }
 
 
-/* Quando um arquivo é pedido para abertura
- * ex: vim arquivo
+/*
+ * As operações em arquivos
+ */
+
+/* abertura de um arquivo
  */
 static int vsfs_open(struct inode *inode, struct file *filp)
 {
      return 0;
 }
 
-
-/* Quando é pedido para fazer uma leitura no arquivo
+/* leitura de arquivo
  * cat arquivo
  */
 static ssize_t vsfs_read_file(struct file *filp, char *buf,
@@ -73,34 +104,64 @@ static ssize_t vsfs_read_file(struct file *filp, char *buf,
     return count;
 }
 
-/* Escrita do arquivo:
+/*escrever um arquivo
  *
  */
 static ssize_t vsfs_write_file(struct file *filp, const char *buff,
                                             size_t len, loff_t *off)
 {
-   printk ("Sorry, this operation isn't supported.\n");
-   return -EINVAL;
+    printk ("Operação não implementada.\n");
+    return -EINVAL;
 }
 
-/* Release */
-static int vsfs_release(struct inode *inode, struct file *file)
+
+/*
+ * SuperBlocks
+ */
+
+static struct super_operations const vsfs_super_ops = {
+    .put_super = vsfs_put_super,
+    //.delete_inode = vsfs_inode_delete,
+    //.alloc_inode = vsfs_inode_alloc,
+};
+
+static void vsfs_put_super(struct super_block *sb)
 {
-   return 0;
+    pr_debug("aufs super block destroyed\n");
 }
 
+/* Completa um superblock
+ * a função simple_fill_super() é a função que faz isso mais "facilmente"
+ */
 static int vsfs_fill_super (struct super_block *sb, void *data, int silent)
 {
     return simple_fill_super(sb, VSFS_MAGIC, vsfs);
 }
-
-static struct dentry *vsfs_get_super(struct file_system_type *fst,
+/* Para registrar um superbloco do filesystem
+ */
+static struct dentry *vsfs_mount(struct file_system_type *fst,
 					     int flags, const char *devname, void *data)
 {
-    return mount_bdev(fst, flags, devname, data, vsfs_fill_super);
+
+    struct dentry *mount = mount_bdev(fst, flags, devname, data,
+                                                    vsfs_fill_super);
+
+    if ( IS_ERR(mount) )
+        printk(KERN_ERR "[Erro] Não foi possível montar VSFS");
+    else
+        printk(KERN_INFO " VSFS montado em %s\n", devname);
+
+    return mount;
 }
+
+static void vsfs_umount(struct super_block *sb)
+{
+	printk(KERN_INFO "VSFS desmontado\n");
+	/* This is just a dummy function as of now. As our filesystem gets matured,
+	 * we will do more meaningful operations here */
+	return kill_litter_super(sb);
+}
+
 
 module_init(vsfs_init);
 module_exit(vsfs_exit);
-
-MODULE_LICENSE("GPL");
--- projeto-final/vsfs.h
+++ projeto-final/vsfs.h
@@ -4,14 +4,30 @@
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 
+
+/*
+ * Operações em arquivos
+ */
+
+/* abertura de um arquivo */
 static int vsfs_open(struct inode *inode, struct file *filp);
-static ssize_t vsfs_write_file(struct file *filp, const char *buff,
-                                            size_t len, loff_t *off);
+/* leitura de arquivo  */
 static ssize_t vsfs_read_file(struct file *filp, char *buf,
                                         size_t count, loff_t *offset);
+/*escrever um arquivo */
 static ssize_t vsfs_write_file(struct file *filp, const char *buff,
                                             size_t len, loff_t *off);
 
-static int vsfs_release(struct inode *inode, struct file *file);
-static struct dentry *vsfs_get_super(struct file_system_type *fst,
+
+/*
+ * Superblocks
+ */
+static void vsfs_put_super(struct super_block *sb);
+static int vsfs_fill_super (struct super_block *sb, void *data, int silent);
+static struct dentry *vsfs_mount(struct file_system_type *fst,
 					     int flags, const char *devname, void *data);
+static void vsfs_umount(struct super_block *sb);
+
+/*
+ * Inodes
+ */
